bbe835d895441564be9867d7d79a3753590bee01
Honey Agrawal <114687023+honeyagrawal21@users.noreply.github.com>
2025-05-21 16:18:03 +0000

    container change done

---
+++ 01_basic/03_data_types.js
@@ -0,0 +1,17 @@
+"use strict" //treat all js code as newer version
+// alert("hello")//we are using nodejs not browser
+/* data types
+1)string
+2)number
+3)boolean
+4)symbol->unique
+5)bigint
+6)null -> standalone value -> representation of empty value
+7)undefined
+*/
+let string = "honey"
+console.log(typeof null)
+console.log(typeof undefined)
+
+
+
