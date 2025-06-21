17e68f879567d3786fe5805f9a6ee99dc1f0635b
Honey Agrawal <114687023+honeyagrawal21@users.noreply.github.com>
2025-06-21 05:41:54 +0000

    container config done

---
+++ 01_basic/04_operations.js
@@ -0,0 +1,18 @@
+let value = 2
+let negvalue = -value
+// console.log(negvalue);
+
+let str1 = "Hiii"
+let str2 = "Hello"
+let str3 = str1 + str2
+// console.log(str3);
+
+console.log(1+2+"4");
+console.log("2"+4+5);
+console.log("2"+"4");
+console.log(2+4);
+console.log(+true);
+console.log(+"");
+let no = 1;
+++no;
+console.log(no);
