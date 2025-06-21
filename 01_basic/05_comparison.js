93a4356594c5b1a87bf98a02e07347f619477869
Honey Agrawal <114687023+honeyagrawal21@users.noreply.github.com>
2025-06-21 06:11:55 +0000

    container config done

---
+++ 01_basic/05_comparison.js
@@ -0,0 +1,12 @@
+console.log("02">1);
+console.log(null>0); //comparison operator >,<,>=,<= convert null to a number which is 0
+console.log(null==0); // equality check do not convert null into 0
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+// === (strictly check)
+console.log("2"==2);
+console.log("2"===2)
